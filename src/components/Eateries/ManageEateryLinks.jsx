93aeb85e9ed6919a8a328228abca40fd55926f15
Hamza Zbili <hamza.zbili.92@gmail.com>
2022-09-21 18:18:12 +0200

    refactored accountEatery component

---
+++ src/components/Eateries/ManageEateryLinks.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import DeleteEatery from "../Forms/DeleteEatery";
+import "./myEateries.css";
+
+const MyEateryLinks = ({
+  label,
+  eateries,
+  updateEateries,
+  text,
+  hasManageLink = true,
+}) => {
+  return (
+    <>
+      <h3>{label}</h3>
+      <div className="myEateries">
+        {eateries?.map((eatery) => {
+          return (
+            <div key={eatery._id}>
+              <Link to={`/eateries/join/${eatery._id}`}>
+                {eatery.businessName}
+              </Link>
+              <DeleteEatery id={eatery._id} updateEateries={updateEateries} />
+              {hasManageLink && (
+                <Link to={`/eateries/join/${eatery._id}`}>{text}</Link>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default MyEateryLinks;
--- src/pages/EateryAccount.jsx
+++ src/pages/EateryAccount.jsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useState } from "react";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import MyEateryReview from "../components/Eateries/MyEateryReview";
+import ManageEateryLinks from "../components/Eateries/ManageEateryLinks";
 import BackButton from "../components/Navbar/BackButton";
 import useAuth from "../context/auth/useAuth";
 import service from "../services/apiHandler";
@@ -27,96 +27,31 @@ const EateryAccount = () => {
       </div>
     );
 
-  const reviewEateries = userEateries.filter(
-    (eatery) => eatery.isReputable === "review"
-  );
-  const reputableEateries = userEateries.filter(
-    (eatery) => eatery.isReputable === "repu-table"
-  );
-  const pendingEateries = userEateries.filter(
-    (eatery) => eatery.isReputable === "pending"
-  );
-  const yetToApplyEateries = userEateries.filter(
-    (eatery) => eatery.isReputable === "false"
-  );
+  const eateryCollections = [
+    { label: "reputable eateries", status: "repu-table" },
+    {
+      label: "please review",
+      status: "review",
+      text: "review",
+      hasManageLink: false,
+    },
+    { label: "application sent", status: "pending" },
+    { label: "yet to apply", status: "false" },
+  ];
 
   return (
     <div className="accountContainer">
       <BackButton />
       {currentUser && <h5>Hi {currentUser.username}!</h5>}
-      <div>
-        {reputableEateries.length > 0 && (
-          <div>
-            <h3>reputable eateries</h3>
-            {reputableEateries.map((eatery) => {
-              return (
-                <MyEateryReview
-                  updateEateries={updateEateries}
-                  eatery={eatery}
-                  key={eatery._id}
-                  hasManageLink={false}
-                />
-              );
-            })}
-          </div>
-        )}
-      </div>
-      <div>
-        {reviewEateries.length > 0 ? (
-          <div>
-            <h3>please review</h3>
-            {reviewEateries.map((eatery) => {
-              return (
-                <MyEateryReview
-                  updateEateries={updateEateries}
-                  eatery={eatery}
-                  key={eatery._id}
-                  moderatorMessage={eatery.moderatorMessage}
-                />
-              );
-            })}
-          </div>
-        ) : (
-          ""
-        )}
-      </div>
-      <div>
-        {pendingEateries.length > 0 ? (
-          <div>
-            <h3>application sent</h3>
-            {pendingEateries.map((eatery) => {
-              return (
-                <MyEateryReview
-                  updateEateries={updateEateries}
-                  eatery={eatery}
-                  key={eatery._id}
-                />
-              );
-            })}
-          </div>
-        ) : (
-          ""
-        )}
-      </div>
-      <div>
-        {yetToApplyEateries.length > 0 ? (
-          <div>
-            <h3>yet to apply</h3>
-            {yetToApplyEateries.map((eatery) => {
-              return (
-                <MyEateryReview
-                  updateEateries={updateEateries}
-                  eatery={eatery}
-                  key={eatery._id}
-                  text={"apply"}
-                />
-              );
-            })}
-          </div>
-        ) : (
-          ""
-        )}
-      </div>
+      {eateryCollections.map(({ status, ...collectionProps }) => (
+        <ManageEateryLinks
+          eateries={userEateries.filter(
+            ({ isReputable }) => isReputable === status
+          )}
+          updateEateries={updateEateries}
+          {...collectionProps}
+        />
+      ))}
       <Link to="/eateries/my/new">post a new eatery</Link>
     </div>
   );
